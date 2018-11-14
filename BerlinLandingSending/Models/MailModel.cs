fcfda0311d549f8623dd4eedacb5d2e356d34a13
PostolenkoS <Ravovis@gmail.com>
2018-11-14 16:21:49 +0200

    second commit

---
+++ BerlinLandingSending/Models/MailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BerlinLandingSending.Models
+{
+    public class MailModel
+    {
+        public string Name { get; set; }
+        public string EMail { get; set; }
+        public string Text { get; set; }
+    }
+}
